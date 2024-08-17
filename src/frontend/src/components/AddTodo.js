43712b9db31bb49fe5044ea48c491fd9688699ca
Tuong Le <tuong.levan@nashtechglobal.com>
2024-08-17 19:37:34 +0700

    - Change Button name of frontend from "Add todo" to "Add Todo Webhook"
    - Change Button name of frontend from "Add Todo Webhook" to "Add Todo trigger Webhook"

--- src/frontend/src/components/AddTodo.js
+++ src/frontend/src/components/AddTodo.js
@@ -25,7 +25,7 @@ export default class AddTodo extends React.Component {
           className="form-control"
         />
         <button className="btn btn-primary" type="submit">
-          Add Todo
+          Add Todo trigger Webhook
         </button>
       </form>
     );
